8e7751e925a931c9510848c70909e72e97a00f10
xiyuan_fengyu <xiyuan_fengyu@163.com>
2019-08-17 01:08:53 +0800

    1153 代码已完成 LeetCode提交校验过程有bug，测试用例["abcdefghijklmnopqrstuvwxyz", "bcdefghijklmnopqrstuvwxyzq"]输出结果始终为false，但是本地测试以及线上自定义测试用例都可以输出正确答案；即使检测特殊条件后直接返回true，在提交过程中，仍然显示输出为false

--- src/main/java/leetcode/_1153_H.java
+++ src/main/java/leetcode/_1153_H.java
@@ -2,6 +2,8 @@ package leetcode;
 
 import com.xiyuan.templateString.EnableTemplateString;
 
+import java.util.Arrays;
+
 import static com.xiyuan.templateString.TemplateString.S.r;
 
 /**
@@ -32,11 +34,105 @@ public class _1153_H {
     str1 和 str2 中都只会出现 小写英文字母
      */
 
+    /**
+     * 解题思路 http://note.youdao.com/noteshare?id=214f587d7e7fa79cdd5fdaf39ba0630c&sub=36456EC3F53C47A29F8EA0D90177B2E4
+     */
     class Solution {
 
         public boolean canConvert(String str1, String str2) {
+            int len = str1.length();
+            if (len != str2.length()) {
+                return false;
+            }
+            if (len <= 1) {
+                return true;
+            }
 
-            return false;
+            char[] chars1 = str1.toCharArray();
+            char[] chars2 = str2.toCharArray();
+
+            // 迁移表
+            char[] table = new char['z' + 1];
+            // 第一次更新为上游字母，发现第二个上游字母后，将值更新为 1，用于后面用来判定环上是否存在上游侧链
+            char[] upperReaches = new char['z' + 1];
+            // 下面 visited 用于辅助计算空闲字母数量
+            boolean[] visited = new boolean['z' + 1];
+            int freeCharNum = 26;
+            for (int i = 0; i < len; i++) {
+                char char1 = chars1[i];
+                char char2 = chars2[i];
+
+                if (!visited[char1]) {
+                    visited[char1] = true;
+                    freeCharNum--;
+                }
+                if (!visited[char2]) {
+                    visited[char2] = true;
+                    freeCharNum--;
+                }
+
+                if (table[char1] != 0 && table[char1] != char2) {
+                    // 该字母下游分叉，不可转换
+                    return false;
+                }
+                else {
+                    // 更新 char2 的上游字母情况
+                    char upperReach = upperReaches[char2];
+                    if (upperReach == 0) {
+                        upperReaches[char2] = char1;
+                    }
+                    else if (upperReach >= 'a' && upperReach != char1) {
+                        // 有上游侧链
+                        upperReaches[char2] = 1;
+                    }
+                    table[char1] = char2;
+                }
+            }
+
+            // 检查是否存在环
+            // 下面 visited 用于辅助记录哪些字母是访问过的
+            Arrays.fill(visited, false);
+            for (char i = 'a'; i <= 'z'; i++) {
+                if (visited[i]) {
+                    continue;
+                }
+                // 检测环的过程中记录是否有上游侧链
+                boolean hasUpperSideLine = false;
+                boolean hasCircle = false;
+                char nextC = i;
+                // 检查是否有环
+                while ((nextC = table[nextC]) != 0) {
+                    if (visited[nextC]) {
+                        // 已检查过的节点，认为可以走通转换流程
+                        break;
+                    }
+
+                    if (upperReaches[nextC] == 1)  {
+                        hasUpperSideLine = true;
+                    }
+
+                    if (nextC == i) {
+                        // 有环
+                        hasCircle = true;
+                        break;
+                    }
+
+                    visited[nextC] = true;
+                }
+                visited[i] = true;
+
+                if (hasCircle) {
+                    // 有环
+                    if (hasUpperSideLine || freeCharNum > 0) {
+                        // 存在上游侧链 或 有空闲字母，可以转换
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
 
     }
@@ -44,9 +140,9 @@ public class _1153_H {
     public static void main(String[] args) {
         Tester.test(
                 r(/*
-                ["Solution"]
-                [[]]
-                [null]
+                ["Solution", "canConvert", "canConvert", "canConvert", "canConvert", "canConvert"]
+                [[], ["aabcc", "ccdee"], ["leetcode", "codeleet"], ["abcdefghijklmnopqrstuvwxyz", "zabcdefghijklmnopqrstuvwxy"], ["abcdefghijklmnopqrstuvwxyz", "bcdefghijklmnopqrstuvwxyzr"], ["abcdefghijklmnopqrstuvwxyz", "bcdefghijklmnopqrstuvwxyzq"]]
+                [null, true, false, false, true, true]
                 */)
         );
     }
