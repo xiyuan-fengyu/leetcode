26d6d808533228ee0582c5aed0092164da0f8f92
xiyuan_fengyu <xiyuan_fengyu@163.com>
2019-08-16 10:56:17 +0800

    1151 AC 6ms

--- src/main/java/leetcode/_1150_E.java
+++ src/main/java/leetcode/_1150_E.java
@@ -6,6 +6,7 @@ import static com.xiyuan.templateString.TemplateString.S.r;
 
 /**
  * Created by xiyuan_fengyu on 2019/8/16 9:39.
+ * https://leetcode-cn.com/contest/biweekly-contest-6/problems/check-if-a-number-is-majority-element-in-a-sorted-array/
  */
 @EnableTemplateString
 public class _1150_E {
--- src/main/java/leetcode/_1151_M.java
+++ src/main/java/leetcode/_1151_M.java
@@ -6,14 +6,34 @@ import static com.xiyuan.templateString.TemplateString.S.r;
 
 /**
  * Created by xiyuan_fengyu on 2019/8/16 10:13.
+ * https://leetcode-cn.com/contest/biweekly-contest-6/problems/minimum-swaps-to-group-all-1s-together/
  */
 @EnableTemplateString
 public class _1151_M {
 
     class Solution {
         public int minSwaps(int[] data) {
-
-            return 0;
+            int countOfOne = 0;
+            for (int i = 0; i < data.length; i++) {
+                // 因为 0 <= data[i] <= 1，所以这里可以通过加上 data[i] 来统计 1 出现的次数，下面几处同理
+                countOfOne += data[i];
+            }
+            // 用一个大小为 countOfOne 的窗口从左往右移动，计算窗口内 1 的数量的最大值
+            int maxCountOfOneInWindow = 0;
+            for (int i = 0; i < countOfOne; i++) {
+                maxCountOfOneInWindow += data[i];
+            }
+            int countOfOneInWindow = maxCountOfOneInWindow;
+            for (int i = countOfOne; i < data.length; i++) {
+                // 窗口右边新增1
+                // 窗口左边移除1
+                countOfOneInWindow += data[i] - data[i - countOfOne];
+                if (countOfOneInWindow > maxCountOfOneInWindow) {
+                    maxCountOfOneInWindow = countOfOneInWindow;
+                }
+            }
+            // 将其他 1 移动到含 1 最多的窗口，即为最少移动次数
+            return countOfOne - maxCountOfOneInWindow;
         }
     }
 
