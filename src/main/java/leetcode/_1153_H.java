63623df187b965cf9519e9628b1c988bb080ba1b
xiyuan_fengyu <xiyuan_fengyu@163.com>
2019-08-16 17:53:10 +0800

    1153 todo

---
+++ src/main/java/leetcode/_1153_H.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import com.xiyuan.templateString.EnableTemplateString;
+
+import static com.xiyuan.templateString.TemplateString.S.r;
+
+/**
+ * Created by xiyuan_fengyu on 2019/8/16 17:44.
+ * https://leetcode-cn.com/contest/biweekly-contest-6/problems/string-transforms-into-another-string/
+ */
+@EnableTemplateString
+public class _1153_H {
+
+    class Solution {
+
+        public boolean canConvert(String str1, String str2) {
+
+            return false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Tester.test(
+                r(/*
+                ["Solution"]
+                [[]]
+                [null]
+                */)
+        );
+    }
+
+}
